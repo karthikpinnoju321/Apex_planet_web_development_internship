b76c4662cc86fb144627f72c0d0ede06b52aefec
karthikpinnoju321 <karthikpinnoju321@gmail.com>
2026-07-10 17:49:06 +0530

    Create script.js

---
+++ Task 1/script.js
@@ -0,0 +1,10 @@
+function showMessage(){
+
+    alert("Welcome Karthik! 🎉\nYou successfully used JavaScript.");
+
+}
+
+const today = new Date();
+
+document.getElementById("date").innerHTML =
+"Today's Date : " + today.toDateString();
